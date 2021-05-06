4570e9d0d482e26caec9696b3f5f1c6e96b7992f
wpmcgrath95 <wpmcgrath95@gmail.com>
2021-05-05 21:54:14 -0700

    modified python file

--- python/linear_reg_model.py
+++ python/linear_reg_model.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import sys
+
+from linear_model import LinearModelScratch
+
+
+class LinearRegressionScratch:
+    def __init__(self):
+        pass
+
+    def main(self):
+        LinearModelScratch().main()
+
+
+if __name__ == "__main__":
+    sys.exit(LinearRegressionScratch().main())
